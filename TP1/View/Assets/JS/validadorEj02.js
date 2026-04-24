4bd7bce450851f8d426b3f9726e02e53b9e863e2
Pablit081 <pabariled@gmail.com>
2026-04-24 16:24:43 -0300

    refactor: simplifica validadorEj02 usando funciones globales

--- TP1/View/Assets/JS/validadorEj02.js
+++ TP1/View/Assets/JS/validadorEj02.js
@@ -1,21 +1,3 @@
-// --- FUNCIONES AUXILIARES (Plantilla de la cátedra) ---
-function marcarInvalido(input, mensaje) {
-    input.classList.remove("is-valid");
-    input.classList.add("is-invalid");
-    let feedback = input.parentElement.querySelector(".invalid-feedback");
-    if (!feedback) {
-        feedback = document.createElement("div");
-        feedback.classList.add("invalid-feedback");
-        input.insertAdjacentElement("afterend", feedback);
-    }
-    feedback.textContent = mensaje;
-}
-
-function marcarValido(input) {
-    input.classList.remove("is-invalid");
-    input.classList.add("is-valid");
-}
-
 // --- LÓGICA PARA ARREGLOS DE INPUTS ---
 document.addEventListener("DOMContentLoaded", () => {
     const formulario = document.getElementById("formEj02");
