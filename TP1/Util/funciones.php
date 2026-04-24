62157c11ae3171559ab7e922e3d5d80b28a8ed9e
Pablit081 <pabariled@gmail.com>
2026-04-24 16:20:41 -0300

    feat: agrega función esMayorDeEdad para lógica de negocio

--- TP1/Util/funciones.php
+++ TP1/Util/funciones.php
@@ -36,6 +36,15 @@ function generarSaludo($nombre, $apellido, $edad, $direccion) {
     return "Hola, yo soy $nombre $apellido, tengo $edad años y vivo en $direccion.";
 }
 
+/**
+ * Determina si una persona es mayor de edad.
+ * Ejercicio 4
+ */
+function esMayorDeEdad($edad) {
+    // La consigna dice: mayor o igual a 18
+    return ($edad >= 18);
+}
+
 
 // Siguiente funcion de los demás ejercicios...
 
